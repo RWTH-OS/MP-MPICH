e3d154e71707517666d4ced46c3f4a53eec15e4f
rainer <rainer@3ddfa0b1-c7ec-0310-823c-dd91aa7df4cc>
2006-11-30 17:56:43 +0000

    Updated version information which is printed when running programs with -mpiversion
    git-svn-id: https://svn.lfbs.rwth-aachen.de/svn/mp-mpich/branches/rc-1.5@4955 3ddfa0b1-c7ec-0310-823c-dd91aa7df4cc

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -3,11 +3,11 @@
    Change and commit this file for a new release. The version information 
    specified here will be printed using the -mpiversion option with an application. */
 
-#define PATCHLEVEL 1.3
+#define PATCHLEVEL 1.5
 #define PATCHLEVEL_MAJOR 1
-#define PATCHLEVEL_MINOR 3
+#define PATCHLEVEL_MINOR 5
 #define PATCHLEVEL_SUBMINOR 0
-#define PATCHLEVEL_RELEASE_KIND "b"
+#define PATCHLEVEL_RELEASE_KIND ""
 #ifndef PATCHLEVEL_RELEASE_DATE 
 #ifdef RELEASE_DATE
 #define PATCHLEVEL_RELEASE_DATE RELEASE_DATE
